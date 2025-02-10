3ba92c1f5c9033684539b6bd9b0c4dfaef094207
hanzbzz <honza.brazda2@seznam.cz>
2025-02-10 22:03:50 +0100

    fix add encryptionCert to runc-criu options

--- checkpoint.go
+++ checkpoint.go
@@ -43,7 +43,7 @@ checkpointed.`,
 		cli.StringSliceFlag{Name: "empty-ns", Usage: "create a namespace, but don't restore its properties"},
 		cli.BoolFlag{Name: "auto-dedup", Usage: "enable auto deduplication of memory images"},
 		cli.BoolFlag{Name: "encrypt", Usage: "encrypt the resuling checkpoint"},
-		cli.StringFlag{Name: "encryption-cert", Usage: "location of the public cert to use for encryption"},
+		cli.StringFlag{Name: "encryption-cert", Value: "", Usage: "location of the public cert to use for encryption"},
 	},
 	Action: func(context *cli.Context) error {
 		if err := checkArgs(context, 1, exactArgs); err != nil {
@@ -135,6 +135,7 @@ func criuOptions(context *cli.Context) (*libcontainer.CriuOpts, error) {
 		LsmProfile:              context.String("lsm-profile"),
 		LsmMountContext:         context.String("lsm-mount-context"),
 		Encrypt:                 context.Bool("encrypt"),
+		EncryptionCert:          context.String("encryption-cert"),
 	}
 
 	// CRIU options below may or may not be set.
